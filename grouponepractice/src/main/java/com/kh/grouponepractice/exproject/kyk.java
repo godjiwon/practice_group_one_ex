6b264790765ab1caa953c7a7350d3960e2e48767
buba2@naver.com <buba2@naver.com>
2024-02-20 17:29:48 +0900

    도와주세요

--- grouponepractice/src/main/java/com/kh/grouponepractice/exproject/kyk.java
+++ grouponepractice/src/main/java/com/kh/grouponepractice/exproject/kyk.java
@@ -1,10 +1,9 @@
 package com.kh.grouponepractice.exproject;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/member")
 public class kyk {
 	public String join() {
 		return "/WEB-INF/views/member/join.jsp";
 	}
+	//도와주세요
 }
