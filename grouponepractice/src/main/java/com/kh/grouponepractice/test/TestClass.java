23c4c87928d478174a84317218856f0281ddac4d
lovejr1114 <lovejr1114@naver.com>
2024-03-08 17:39:12 +0900

    테스트 컨트롤러 클래스

---
+++ grouponepractice/src/main/java/com/kh/grouponepractice/test/TestClass.java
@@ -0,0 +1,14 @@
+package com.kh.grouponepractice.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestClass {
+	
+	@RequestMapping("/testPage")
+	public String testPage() {
+		return "테스트용 1 홈페이지 만들기";
+	}
+		
+}
